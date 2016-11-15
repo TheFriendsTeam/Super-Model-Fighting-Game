3edd2f13a03544e6d85db230d75c6a37f42492c4
peusoigne <peusoigne@gmail.com>
2016-11-14 22:30:33 -0800

    unbroke a thing or 2 in main menu

--- js/MainMenu.js
+++ js/MainMenu.js
@@ -1,18 +1,16 @@
 var TopDownGame = TopDownGame || {};
 
-Template.MainMenu = function() {};
+TopDownGame.MainMenu = function() {};
 
-Template.MainMenu.prototype = {
+TopDownGame.MainMenu.prototype = {
     
     create: function() {
         // tile sprite lets you tile the image as manytimes as you like... I think it make the 
         this.splash = this.add.sprite(this.game.world.centerX, this.game.world.centerY, "logo");
         this.splash.anchor.setTo(0.5);
         
-    },
-    
-    
-    update: function() {
+    }
+    ,update: function() {
         if(this.game.input.activePointer.justPressed()){
             this.state.start("Game");
         }

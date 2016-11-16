714568e046236355ef7b4266d2de93f67d0ea5ce
peusoigne <sarah@missionbits-MacBook-Air-4.local>
2016-11-15 17:48:59 -0800

    image fixing

--- js/MainMenu.js
+++ js/MainMenu.js
@@ -7,10 +7,11 @@ TopDownGame.MainMenu.prototype = {
         // tile sprite lets you tile the image as manytimes as you like... I think it make the 
         this.splash = this.add.sprite(this.game.world.centerX, this.game.world.centerY, "MainMenuBG");
         this.splash.scale.setTo(0.5);
-        this.game.add.tileSprite(0, 0, -400, 100, "MainMenuBG");
         this.splash.anchor.setTo(0.5);
         
     },
+    
+    
     
     
     update: function() {

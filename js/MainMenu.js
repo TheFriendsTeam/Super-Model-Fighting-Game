a22b77a1db45cfde198c762b5eeff23449a03e3e
peusoigne <sarah@missionbits-MacBook-Air-4.local>
2016-12-11 13:16:25 -0800

    ok

--- js/MainMenu.js
+++ js/MainMenu.js
@@ -2,7 +2,6 @@ var TopDownGame = TopDownGame || {};
 TopDownGame.MainMenu = function () {};
 TopDownGame.MainMenu.prototype = {
     create: function () {
-        // tile sprite lets you tile the image as manytimes as you like... I think it make the 
         this.splash = this.add.sprite(this.game.world.centerX, this.game.world.centerY, "SUPERMODEL");
         this.splash.anchor.setTo(0.5);
         var clic = this.game.add.button(this.game.world.centerX, this.game.world.centerY, "tras", this.actionOnClick, this, 2, 1, 0);
